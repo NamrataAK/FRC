757bc5663257d33efe40469febc7f57eecd16872
NamrataAK <amanenk10@gmail.com>
2020-05-19 08:32:45 +0530

    commited

--- Online-Food-Ordering-System/proj.py
+++ Online-Food-Ordering-System/proj.py
@@ -6,11 +6,115 @@ import cgitb; cgitb.enable()
 #from werkzeug import secure_filename
 import time
 import collections
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import pairwise_distances
+import heapq
 
 
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
+
+###########################################################################################################
+
+# Data downloaded from https://grouplens.org/datasets/movielens/100k/
+
+#Reading users file:
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,
+ encoding='latin-1')
+
+#Reading ratings file:
+r_cols = ['user_id', 'dish_id', 'rating', 'unix_timestamp']
+ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,
+ encoding='latin-1')
+
+#Reading items file:
+i_cols = ['Dish id', 'Dish Name', 'Nutrition Value', 'Food Information', 'Unknown', 'Salad', 'Breads','Curry','Rice', 'Veggies', 'Regular Mini Meal', 'Regular Full Meal', 'Special Mini Meal', 'Special Full Meal',
+ 'Protein mini meal', 'Protein full meal']
+items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,
+ encoding='latin-1')
+ 
+###########################################################################################################
+
+print(users.shape)
+print(users.head(25))
+
+###########################################################################################################
+
+print(ratings.shape)
+print(ratings.head(25))
+
+###########################################################################################################
+
+print(items.shape)
+print(items.head(25))
+
+###########################################################################################################
+
+r_cols = ['user_id', 'dish_id', 'rating', 'unix_timestamp']
+ratings_train = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
+ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
+ratings_train.shape, ratings_test.shape
+print(ratings_train.head(25))
+
+###########################################################################################################
+
+n_users = ratings.user_id.shape[0]
+n_items = ratings.dish_id.unique().shape[0]
+print(n_users, n_items)
+
+###########################################################################################################
+
+data_matrix = np.zeros((n_users, n_items))
+for line in ratings.itertuples():
+    data_matrix[line[1]-1, line[2]-1] = line[3]
+
+###########################################################################################################
+
+user_similarity = pairwise_distances(data_matrix, metric='cosine')
+item_similarity = pairwise_distances(data_matrix.T, metric='cosine')
+print(user_similarity.shape, item_similarity.shape)
+print(user_similarity)
+print(item_similarity)
+
+###########################################################################################################
+
+def predict(ratings, similarity, type='user'):
+    if type == 'user':
+        mean_user_rating = ratings.mean(axis=1)
+        #We use np.newaxis so that mean_user_rating has same format as ratings
+        ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
+        pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
+    elif type == 'item':
+        pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+    return pred
+
+###########################################################################################################
+
+user_prediction = predict(data_matrix, user_similarity, type='user')
+item_prediction = predict(data_matrix, item_similarity, type='item')
+####print(user_prediction.shape, item_prediction.shape)
+
+print(user_prediction)
+print(item_prediction)
+
+###########################################################################################################
+
+
+
+
+GGG = 1
+user_item_list_m = list(item_prediction[GGG])
+rec_list = heapq.nlargest(20,(user_item_list_m))
+print(rec_list)
+
+
+rec_index = []
+for elementss in rec_list:
+    rec_index.append(user_item_list_m.index(elementss))
+print(rec_index)
 
 #homepage
 @app.route('/')
@@ -66,28 +170,48 @@ def customer_logout():
 def homepage_customer():
 	conn=sqlite3.connect('members.db')
 	cur=conn.cursor()
-	cur.execute("SELECT * FROM response WHERE username=?",(session['username'],))
+	cur.execute("SELECT * FROM customers WHERE username=?",(session['username'],))
 	response=cur.fetchall()
-	cur.execute("SELECT * FROM most_ordered ORDER BY orders DESC limit 4")
+	cur.execute("SELECT * FROM most_ordered ORDER BY item ASC limit 6")
 	most_ordered=cur.fetchall()
 	conn.close()
 	return render_template('homepage_customer.html',response=response,most_ordered=most_ordered)
+    
 
 
 
 #to show items in cart
 @app.route('/liked')
 def liked():
-	return render_template('liked.html')
+	conn=sqlite3.connect('members.db')
+	cur=conn.cursor()
+	cur.execute("SELECT * FROM response WHERE username=?",(session['username'],))
+	response=cur.fetchall()
+	cur.execute("SELECT * FROM most_ordered ORDER BY item ASC limit 6")
+	most_ordered=cur.fetchall()
+	conn.close()
+	return render_template('liked.html',response=response,most_ordered=most_ordered)
 
-@app.route('/purchased')
-def purchased():
-	return render_template('purchased.html')
+@app.route('/Nutrition')
+def Nutrition():
+	conn=sqlite3.connect('members.db')
+	cur=conn.cursor()
+	cur.execute("SELECT * FROM response WHERE username=?",(session['username'],))
+	response=cur.fetchall()
+	cur.execute("SELECT * FROM most_ordered ORDER BY item ASC limit 6")
+	most_ordered=cur.fetchall()
+	conn.close()
+	return render_template('Nutrition.html',response=response,most_ordered=most_ordered)
 
 @app.route('/search')    
 def search():
 	return render_template('search.html')
 			
+@app.route('/quantity/<place>/<rest>/<item>/<price>/<dish_image>',methods=['GET','POST'])
+def quantity(place,rest,item,price,dish_image):
+	return render_template('quantity.html',item=item,price=price,place=place,rest=rest,dish_image=dish_image)
+	#from quantity.html , goes to /postquantity/<item>/<price>
+
 
 
 
